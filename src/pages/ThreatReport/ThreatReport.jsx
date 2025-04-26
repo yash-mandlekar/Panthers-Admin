0c2a70c035a854a18f0aa6333be1cd3cfe711004
yash <ymandlekar9826@gmail.com>
2025-04-26 16:09:30 +0530

    Refactor code structure for improved readability and maintainability

--- src/pages/ThreatReport/ThreatReport.jsx
+++ src/pages/ThreatReport/ThreatReport.jsx
@@ -12,8 +12,8 @@ export default function ThreatReport() {
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
-        description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Panthers"
+        description="Phishing Url Detector"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12">
